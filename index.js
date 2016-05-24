e612e4b7deac8be73467ab5afda0c0f486c2d462
M.C. van den Boogaart <scola@kvdb.name>
2016-05-24 14:00:53 +0200

    Use factory methods

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const zlib = require('zlib');
+const Decoder = zlib.Gunzip;
+const Encoder = zlib.Gzip;
+const encoding = 'gzip';
+
+function decoder(options) {
+  return {
+    encoding,
+    create: () => new Decoder(options)
+  };
+}
+
+function encoder(options) {
+  return {
+    encoding,
+    create: () => new Encoder(options)
+  };
+}
 
 module.exports = {
-  encoding: 'gzip',
-  Decoder: zlib.Gunzip,
-  Encoder: zlib.Gzip
+  encoding,
+  decoder,
+  encoder,
+  Decoder,
+  Encoder
 };
